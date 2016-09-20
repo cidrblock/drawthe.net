c7381b521577ee950f65898f7366d0e98d97060b
Bradley A. Thornton <bradley.thornton@starbucks.com>
2016-09-20 05:18:08 -0700

    remove node modules

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,25 @@
+var pkgjson = require('./package.json');
+
 module.exports = function(grunt) {
 
   grunt.initConfig({
+    copy: {
+      dist: {
+       files: [{
+         expand: true,
+         cwd: 'node_modules/d3',
+         src: 'build/d3.min.js',
+         dest: 'build/js',
+         flatten: true
+       },{
+         expand: true,
+         cwd: 'node_modules/js-yaml',
+         src: 'dist/js-yaml.min.js',
+         dest: 'build/js',
+         flatten: true
+       }]
+     }
+   },
     webfont: {
         icons: {
             src: 'azure/enterprise/*.svg',
@@ -18,8 +37,11 @@ module.exports = function(grunt) {
     },
   });
 
-
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-webfont');
-  grunt.registerTask('default', ['webfont']);
+  grunt.registerTask('default', [
+    'copy',
+    'webfont'
+  ]);
 
 };
