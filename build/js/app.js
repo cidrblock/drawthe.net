bf3dfced9fa1d853d0d56e5191891c396bdd9ae2
Bradley A. Thornton <bradley.thornton@starbucks.com>
2016-10-08 11:25:58 -0700

    add save and update

--- build/js/app.js
+++ build/js/app.js
@@ -14,3 +14,25 @@ function doc_keyUp(e) {
   }
 // register the handler
 document.addEventListener('keyup', doc_keyUp, false);
+
+function copyToClipboard(text) {
+
+  // Create a "hidden" input
+  var aux = document.createElement("input");
+
+  // Assign it the value of the specified element
+  aux.setAttribute("value", text);
+
+  // Append it to the body
+  document.body.appendChild(aux);
+
+  // Highlight its content
+  aux.select();
+
+  // Copy the highlighted text
+  document.execCommand("copy");
+
+  // Remove it from the body
+  document.body.removeChild(aux);
+
+}
