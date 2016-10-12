729bbcbbe5194dae8c08c4165b976d41bbaf407d
Bradley A. Thornton <bradley.thornton@starbucks.com>
2016-10-12 06:22:57 -0700

    metadata

--- build/js/dld4e-icons.js
+++ build/js/dld4e-icons.js
@@ -21,6 +21,8 @@ var drawIcons = function (svg, diagram, icons, iconTextRatio) {
     .append("text")
     .attr('class', 'iconLabel')
     .text( function (d) { return d.value.text || d.key })
+    .on("mouseover", handleMouseOver)
+    .on("mouseout", handleMouseOut)
     .each( function(d) {
       d.value.fontSize = Math.floor(Math.min(d.value.width*.9 / this.getComputedTextLength() * 12, d.value.height/2*iconTextRatio))
       d.value.textPosition = textPositions(0,0,d.value.width,d.value.height,d.value.fontSize + 2)[d.value.textLocation]
@@ -90,5 +92,51 @@ var drawIcons = function (svg, diagram, icons, iconTextRatio) {
       })
 
     })
+
+    function handleMouseOver(d, i) {  // Add interactivity
+
+      console.log(d)
+            // Use D3 to select element, change color and size
+            // d3.select(this).attr({
+            //   fill: "orange",
+            //   r: radius * 2
+            // });
+      var metadata = d.value
+      console.log(metadata.keys.length)
+      var meta = svg
+        .append("foreignObject")
+        .attr("id", "t" + d.x + "-" + d.y + "-" + i)
+        .attr("width", function() { return d.value.width * 2 + "px" })
+        .style("height", function() { return metadata.length * d.value.fontSize })
+        .attr("x", function() { return d.value.x2; })
+        .attr("y", function() { return d.value.centerY - metadata.length * d.value.fontSize })
+
+        .append("xhtml:div")
+        .attr("class", "metadata")
+        .style("width", function() { return d.value.width * 2 + "px" })
+        .style("height", function() { return metadata.length * d.value.fontSize })
+        .style("font-size", function() { return d.value.fontSize + "px"; })
+        .html(function() {
+          var text = "<table>"
+          for (key in d.value) {
+            text += ("<tr><td>" + key + ":&nbsp</td><td>" + d.value[key] + "</td></tr>")
+          }
+          text += "</table>"
+          return text;
+        })
+     // // Specify where to put label of text
+      // svg.append("div")
+      //   .attr("class", "tooltip")
+      //   .style("left", (d.value.x2) + "px")
+      //   .style("top", (d.value.y2) + "px")
+      //   .html("foo")
+      // svg.append("text")
+
+
+
+    }
+    function handleMouseOut(d, i) {
+      //d3.select("#t" + d.x + "-" + d.y + "-" + i).remove();  // Remove text location
+    }
 
 }
